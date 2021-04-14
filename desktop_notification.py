6167afc0bbceb979fdc83ec9c72fc651f3d74ab0
Damyan Doykov <77811406+Damyanmd@users.noreply.github.com>
2021-04-14 22:54:39 +0300

    Add files via upload

---
+++ desktop_notification.py
@@ -0,0 +1,16 @@
+from plyer import notification
+import time
+
+def notify_me(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        # This is an example, you can use the same format for your directory where the icon is placed
+        app_icon="C:\\Users\\User\\Documents\\Programirane\\Projects\\Desktop Notifier App\\eye.ico",
+        timeout=20,
+    )
+
+if __name__ == '__main__':
+    while True:
+        notify_me("Hello User, time for a break!", "It\'s time for 20-20-20 to keep your eyes healthy.")
+        time.sleep(1200)
